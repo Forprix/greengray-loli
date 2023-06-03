9be549ae02cdc60b8bfce2965a380e51f933465c
Forprix <forprix@mail.ru>
2023-06-03 15:19:04 +0300

    hello

--- upload.bat
+++ upload.bat
@@ -2,8 +2,7 @@
 @git remote add origin https://github.com/Forprix/greengray-loli
 @git fetch origin
 @git reset origin/main
-@git add --all
-@git reset -- upload.bat
+@git add --all -- ':(exclude)upload.bat'
 @git status
 @git commit -am "hello"
 @git push
